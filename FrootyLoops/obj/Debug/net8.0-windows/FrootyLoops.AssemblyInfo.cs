3629c59143256363a3659a4e834385ab6c1582bc
Hiikk0 <c.rodkin.anton@student.uzhnu.edu.ua>
2024-06-09 04:30:54 +0300

    Test

--- FrootyLoops/obj/Debug/net8.0-windows/FrootyLoops.AssemblyInfo.cs
+++ FrootyLoops/obj/Debug/net8.0-windows/FrootyLoops.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrootyLoops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa801ef78151c83c08fd0c41004d1247d70a9a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrootyLoops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrootyLoops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
